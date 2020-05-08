a7f0a5789e6cc8c3b439e6518fd13cc5d66ed612
Yuan Meng <hail_to_psyche@berkeley.edu>
2020-05-07 18:24:20 -0700

    Scrappers

---
+++ clean.py
@@ -0,0 +1,195 @@
+import re
+import unidecode
+import string
+from spellchecker import SpellChecker
+import spacy
+
+# Convert to lowercase
+def lower_case(text):
+    return text.lower()
+
+# Expand contractions
+contraction_dict = {
+    "ain't": "is not",
+    "aren't": "are not",
+    "can't": "cannot",
+    "'cause": "because",
+    "could've": "could have",
+    "couldn't": "could not",
+    "didn't": "did not",
+    "doesn't": "does not",
+    "don't": "do not",
+    "hadn't": "had not",
+    "hasn't": "has not",
+    "haven't": "have not",
+    "he'd": "he would",
+    "he'll": "he will",
+    "he's": "he is",
+    "how'd": "how did",
+    "how'd'y": "how do you",
+    "how'll": "how will",
+    "how's": "how is",
+    "i'd": "i would",
+    "i'd've": "i would have",
+    "i'll": "i will",
+    "i'll've": "i will have",
+    "i'm": "i am",
+    "i've": "i have",
+    "isn't": "is not",
+    "it'd": "it would",
+    "it'd've": "it would have",
+    "it'll": "it will",
+    "it'll've": "it will have",
+    "it's": "it is",
+    "let's": "let us",
+    "ma'am": "madam",
+    "mayn't": "may not",
+    "might've": "might have",
+    "mightn't": "might not",
+    "mightn't've": "might not have",
+    "must've": "must have",
+    "mustn't": "must not",
+    "mustn't've": "must not have",
+    "needn't": "need not",
+    "needn't've": "need not have",
+    "o'clock": "of the clock",
+    "oughtn't": "ought not",
+    "oughtn't've": "ought not have",
+    "shan't": "shall not",
+    "sha'n't": "shall not",
+    "shan't've": "shall not have",
+    "she'd": "she would",
+    "she'd've": "she would have",
+    "she'll": "she will",
+    "she'll've": "she will have",
+    "she's": "she is",
+    "should've": "should have",
+    "shouldn't": "should not",
+    "shouldn't've": "should not have",
+    "so've": "so have",
+    "so's": "so as",
+    "this's": "this is",
+    "that'd": "that would",
+    "that'd've": "that would have",
+    "that's": "that is",
+    "there'd": "there would",
+    "there'd've": "there would have",
+    "there's": "there is",
+    "here's": "here is",
+    "they'd": "they would",
+    "they'd've": "they would have",
+    "they'll": "they will",
+    "they'll've": "they will have",
+    "they're": "they are",
+    "they've": "they have",
+    "to've": "to have",
+    "wasn't": "was not",
+    "we'd": "we would",
+    "we'd've": "we would have",
+    "we'll": "we will",
+    "we'll've": "we will have",
+    "we're": "we are",
+    "we've": "we have",
+    "weren't": "were not",
+    "what'll": "what will",
+    "what'll've": "what will have",
+    "what're": "what are",
+    "what's": "what is",
+    "what've": "what have",
+    "when's": "when is",
+    "when've": "when have",
+    "where'd": "where did",
+    "where's": "where is",
+    "where've": "where have",
+    "who'll": "who will",
+    "who'll've": "who will have",
+    "who's": "who is",
+    "who've": "who have",
+    "why's": "why is",
+    "why've": "why have",
+    "will've": "will have",
+    "won't": "will not",
+    "won't've": "will not have",
+    "would've": "would have",
+    "wouldn't": "would not",
+    "wouldn't've": "would not have",
+    "y'all": "you all",
+    "y'all'd": "you all would",
+    "y'all'd've": "you all would have",
+    "y'all're": "you all are",
+    "y'all've": "you all have",
+    "you'd": "you would",
+    "you'd've": "you would have",
+    "you'll": "you will",
+    "you'll've": "you will have",
+    "you're": "you are",
+    "you've": "you have",
+}
+
+
+def _get_contractions(contraction_dict):
+    contraction_re = re.compile("(%s)" % "|".join(contraction_dict.keys()))
+    return contraction_dict, contraction_re
+
+
+contractions, contractions_re = _get_contractions(contraction_dict)
+
+
+def expand_contractions(text):
+    def replace(match):
+        return contractions[match.group(0)]
+
+    return contractions_re.sub(replace, text)
+
+
+# Remove punctuations
+def remove_punct(text):
+    table = str.maketrans(string.punctuation, " " * len(string.punctuation))
+    return text.translate(table)
+
+
+# Remove accents
+def remove_accented_chars(text):
+    text = unidecode.unidecode(text)
+    return text
+
+
+# Replace with correct spellings
+spell = SpellChecker()
+def correct_spellings(text):
+    corrected_text = []
+    misspelled_words = spell.unknown(text.split())
+    for word in text.split():
+        if word in misspelled_words:
+            corrected_text.append(spell.correction(word))
+        else:
+            corrected_text.append(word)
+    return " ".join(corrected_text)
+
+
+# Remove spaces
+def remove_space(text):
+    return text.strip()
+
+
+# All-in-one normalization
+def normalization(text):
+    text = lower_case(text)
+    text = expand_contractions(text)
+    text = remove_punct(text)
+    text = remove_accented_chars(text)
+    text = correct_spellings(text)
+    text = remove_space(text)
+    return text
+
+
+# Remove stop words
+def remove_stopwords(text):
+    tokens = [token.text for token in text if not token.is_stop]
+    return " ".join([token for token in tokens])
+
+
+# Lemmatization
+def lemmatizer(text):
+    tokens = [token.lemma_ for token in text]
+    return ' '.join([token for token in tokens])
